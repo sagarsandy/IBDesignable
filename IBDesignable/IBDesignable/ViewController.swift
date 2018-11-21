b6bd9227f337f932e261a27a0637eea4b6d0941e
Sagar <sagarsandy492@gmail.com>
2018-11-21 08:40:21 +0530

    Base commit

---
+++ IBDesignable/IBDesignable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IBDesignable
+//
+//  Created by Sagar Sandy on 21/11/18.
+//  Copyright © 2018 Sagar Sandy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
